4554f2019474a670b29d0730d2f61ffe1bce5055
Sunder20 <sunder20@gmail.com>
2020-06-21 15:04:58 +0530

    Last stage

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,12 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+config.assets.initialize_on_precompile = false
 
 module Cafeteria
   class Application < Rails::Application
